1e65cce0f062baa409147c6b7b3cb0df49931aed
viveksinghup <viveksinghup.singh@gmail.com>
2024-09-28 19:48:24 +0530

    recent changes

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,55 +1,55 @@
 <template>
-    <div>
-        <div class="container">
-            <div class="py-20 text-center">
-                <h2 class="text-4xl md:text-6xl font-serif text-gradient">
-                    All Projects
-                </h2>
-                <p
-                    class="text-md md:text-lg px-5 w-full md:w-11/12 mx-auto text-gray-10"
-                >
-                    Responsible for implementing improvements and analysis tools
-                    on our webapp/website, Expanding an existing function or
-                    creating a new one, optimize speed and loading times. In our
-                    larger projects, we work in teams where I am the link
-                    between technology and the customer. I also conduct a few
-                    smaller projects where I handle everything from analysis,
-                    frontend development and processing of results and
-                    statistics. Below are the projects on that i had worked with
-                    over the my journey.
-                </p>
-            </div>
-        </div>
-        <div class="container">
-            <div class="bottom-fold">
-                <section class="flex flex-wrap justify-between mt-30">
-                    <div
-                        class="p-card mb-10 md:mb-16"
-                        v-for="(item, index) in project.all.slice().reverse()"
-                        :key="item.id"
-                        @click="openProjectView(item)"
-                        data-aos="fade-up"
-                        data-aos-duration="1000"
-                        data-aos-delay="100"
-                    >
-                        <ProjectCard
-                            :name="item.name"
-                            :image="item.image"
-                            :isOpen="index === 0"
-                        />
-                    </div>
-                </section>
-            </div>
-            <div class="flex justify-center">
-                <router-link class="btn" to="/">Back to Home</router-link>
-            </div>
-        </div>
-        <ProjectView
-            :selectedProject="selectedProject"
-            :customClass="[selectedProject ? 'reveal' : '']"
-            :closeProjectView="closeProjectView"
-        />
+  <div>
+    <div class="container">
+      <div class="py-20 text-center">
+        <h2 class="text-4xl md:text-6xl font-serif text-gradient">
+          All Projects
+        </h2>
+        <p
+          class="text-md md:text-lg px-5 w-full md:w-11/12 mx-auto text-gray-10"
+        >
+          Being responsible for implementing enhancements and analytical tools
+          for our web applications and websites, focusing on expanding existing
+          functionalities and developing new features while optimizing speed and
+          loading times. In larger projects, I act as the bridge between
+          technology and the client, facilitating effective communication and
+          ensuring alignment of goals. Additionally, I manage several smaller
+          projects, overseeing all phases from analysis to frontend development,
+          as well as the processing of results and statistics. Below are the key
+          projects I have contributed to throughout my career.
+        </p>
+      </div>
     </div>
+    <div class="container">
+      <div class="bottom-fold">
+        <section class="flex flex-wrap justify-between mt-30">
+          <div
+            class="p-card mb-10 md:mb-16"
+            v-for="(item, index) in project.all.slice().reverse()"
+            :key="item.id"
+            @click="openProjectView(item)"
+            data-aos="fade-up"
+            data-aos-duration="1000"
+            data-aos-delay="100"
+          >
+            <ProjectCard
+              :name="item.name"
+              :image="item.image"
+              :isOpen="index === 0"
+            />
+          </div>
+        </section>
+      </div>
+      <div class="flex justify-center">
+        <router-link class="btn" to="/">Back to Home</router-link>
+      </div>
+    </div>
+    <ProjectView
+      :selectedProject="selectedProject"
+      :customClass="[selectedProject ? 'reveal' : '']"
+      :closeProjectView="closeProjectView"
+    />
+  </div>
 </template>
 
 <script>
@@ -58,40 +58,40 @@ import ProjectView from "@/components/ProjectView";
 import { project } from "../constants/project";
 
 export default {
-    name: "Projects",
+  name: "Projects",
 
-    components: {
-        ProjectCard,
-        ProjectView,
+  components: {
+    ProjectCard,
+    ProjectView,
+  },
+  data() {
+    return {
+      project,
+      selectedProject: null,
+    };
+  },
+  props: {
+    items: {
+      type: Array,
+      required: true,
+      default: () => [],
     },
-    data() {
-        return {
-            project,
-            selectedProject: null,
-        };
+  },
+  methods: {
+    openProjectView(item) {
+      this.selectedProject = item;
     },
-    props: {
-        items: {
-            type: Array,
-            required: true,
-            default: () => [],
-        },
+    closeProjectView() {
+      this.selectedProject = null;
     },
-    methods: {
-        openProjectView(item) {
-            this.selectedProject = item;
-        },
-        closeProjectView() {
-            this.selectedProject = null;
-        },
-    },
+  },
 };
 </script>
 <style lang="postcss">
 .p-card {
-    width: 48%;
-    @media screen and (max-width: 768px) {
-        @apply max-w-full w-full;
-    }
+  width: 48%;
+  @media screen and (max-width: 768px) {
+    @apply max-w-full w-full;
+  }
 }
 </style>
